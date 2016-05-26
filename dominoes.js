e18952c685f60902366908a0e72830311936d58d
Jim OKelly <jim@devschool.rocks>
2016-05-26 15:45:04 -0500

    stuck on game loop not repeating

--- dominoes.js
+++ dominoes.js
@@ -1,33 +1,10 @@
 (function() {
-  var Bone = require('./lib/bone');
+  var Game  = require('./lib/game');
 
-  var score = function(start) {
-    var scores = [];
+  Game.addPlayer("Jim");
+  Game.addPlayer("Candra");
+  Game.addPlayer("Brett");
 
-    start.traverse(function(bone) {
-      scores.push(bone.score());
-    });
+  Game.start();
 
-    var sum = function(acc, score) { return score + acc; };
-
-    return scores.reduce(sum, 0);
-
-  };
-
-  var sixSix   = new Bone(6, 6);
-  var sixThree = sixSix.link(new Bone(6, 3));
-  var sixTwo   = sixSix.link(new Bone(6, 2));
-
-  var sixFive = sixSix.link(new Bone(5, 6));
- 
-  var zeroZero =  sixThree.link(new Bone(1, 3))
-                          .link(new Bone(1, 0))
-                          .link(new Bone(0, 0));
-
-  var zeroFive = zeroZero.link(new Bone(0, 5));
-  var fiveFour = sixFive.link(new Bone(5, 4));
-  var zeroFour = zeroZero.link(new Bone(0, 4));
-  var zeroSix = sixSix.link(new Bone(0, 6));
-
-  console.log(score(sixSix));
 }());
